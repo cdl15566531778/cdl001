3ffc879091a93700bdc72a4f74ab19f0013a82b0
DESKTOP-DALG7C6\Administrator <657757486@qq.com>
2019-08-12 09:43:07 +0800

    增加main方法

--- springbootTest/src/main/java/com/jbt/dao/impl/UserDaoImpl.java
+++ springbootTest/src/main/java/com/jbt/dao/impl/UserDaoImpl.java
@@ -6,8 +6,19 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class UserDaoImpl  {
 
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>(1);
+        map.put("1","1");
+        map.put("2","2");
+        for (Map.Entry entry : map.entrySet()){
+            System.out.println(entry.getKey()+"   " + entry.getValue());
+        }
+    }
 
 }
